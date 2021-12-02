9d18a09483f8c138690dfed7ce82e571b9eef138
Minh Vu <34432296+DuploMinh@users.noreply.github.com>
2021-12-03 00:15:31 +0700

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-binary = ""
+binary = ""              
 decimal_or_hex = True;
+def decToHex(dec_value):
+    ret_val = ""
+    while dec_value > 0:
+        hex_value=dec_value%16
+        dec_value=dec_value//16
+        ret_val = getHexChar(hex_value) + ret_val
+    return ret_val
+def getHexChar(dec_digit):
+    if dec_digit < 10:
+        return str(dec_digit)
+    if dec_digit == 10:
+        return "A"
+    if dec_digit == 11:
+        return "B"
+    if dec_digit == 12:
+        return "C"
+    if dec_digit == 13:
+        return "D"
+    if dec_digit == 14:
+        return "E"
+    if dec_digit == 15:
+        return "F"
+    return ""
 def on_gesture_shake():
     global decimal_or_hex
     decimal_or_hex = not decimal_or_hex
+    basic.show_string(str(decimal_or_hex))
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 def a_pressed():
     global binary
@@ -15,9 +39,14 @@ def b_pressed():
     basic.show_string(binary)
 input.on_button_pressed(Button.B, b_pressed)
 def ab_pressed():
+    global decToHex
     decimal = int(binary, 2)
+    hexa = decToHex(decimal)
     if decimal_or_hex:
        basic.show_number(decimal) 
-    else:
-        basic.show_number(decimal)
+    elif not decimal_or_hex:
+        basic.show_string(hexa)
 input.on_button_pressed(Button.AB, ab_pressed)
+def on_logo_event_pressed():
+    binary = ""
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
--- main.ts
+++ main.ts
@@ -1,8 +1,52 @@
 let binary = ""
 let decimal_or_hex = true
+function decToHex(dec_value: number): string {
+    let hex_value: any;
+    let ret_val = ""
+    while (dec_value > 0) {
+        hex_value = dec_value % 16
+        dec_value = Math.idiv(dec_value, 16)
+        ret_val = getHexChar(hex_value) + ret_val
+    }
+    return ret_val
+}
+
+function getHexChar(dec_digit: any): string {
+    if (dec_digit < 10) {
+        return "" + dec_digit
+    }
+    
+    if (dec_digit == 10) {
+        return "A"
+    }
+    
+    if (dec_digit == 11) {
+        return "B"
+    }
+    
+    if (dec_digit == 12) {
+        return "C"
+    }
+    
+    if (dec_digit == 13) {
+        return "D"
+    }
+    
+    if (dec_digit == 14) {
+        return "E"
+    }
+    
+    if (dec_digit == 15) {
+        return "F"
+    }
+    
+    return ""
+}
+
 input.onGesture(Gesture.Shake, function on_gesture_shake() {
     
     decimal_or_hex = !decimal_or_hex
+    basic.showString("" + decimal_or_hex)
 })
 input.onButtonPressed(Button.A, function a_pressed() {
     
@@ -15,11 +59,16 @@ input.onButtonPressed(Button.B, function b_pressed() {
     basic.showString(binary)
 })
 input.onButtonPressed(Button.AB, function ab_pressed() {
+    
     let decimal = parseInt(binary, 2)
+    let hexa = decToHex(decimal)
     if (decimal_or_hex) {
         basic.showNumber(decimal)
-    } else {
-        basic.showNumber(decimal)
+    } else if (!decimal_or_hex) {
+        basic.showString(hexa)
     }
     
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
+    let binary = ""
 })
